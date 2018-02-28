7ebb7c99b2dee6f51afe41d60e33825a1b0c4ba1
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-03-01 01:05:14 +0700

    Add OpeningTheme

--- Dodge or Die!!!/Start.java
+++ Dodge or Die!!!/Start.java
@@ -7,12 +7,18 @@ public class Start extends World
         super(748, 826, 1); 
     }
     
+    public static GreenfootSound opening = new GreenfootSound("Opening.mp3");
     public void act()
     {
+        opening.play();
+        start();
+        //Score.score = 0;
+    }
+    
+    public void start(){
         if(Greenfoot.isKeyDown("enter")){
             Greenfoot.setWorld(new MyWorld());
+            opening.stop();
         }
-        
-        //Score.score = 0;
     }
 }
